14da90c8ac59f3a36184e1566329d78ed5c190de
Saad Bakht <rotk13@gmail.com>
2014-01-15 16:20:40 -0500

    Easy - Roman Numerals Completed

--- CodeEval.rb
+++ CodeEval.rb
@@ -987,7 +987,7 @@ end
 def romanNumerals(line)
   num = line.strip.to_i
   ans = ""
-  dic = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
+  dic = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
 
   while num != 0
     dic.to_a.reverse.each do |key,value|
